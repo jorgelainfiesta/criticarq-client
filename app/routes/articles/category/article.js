4543d16ded7af0310701495eaae16790d0fa9180
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-05-01 20:59:58 -0600

    Fix to redirect to right category when using the wrong URL path.

--- app/routes/articles/category/article.js
+++ app/routes/articles/category/article.js
@@ -8,7 +8,11 @@ export default Route.extend({
   model(params) {
     const categoryArticles = this.modelFor('articles.category');
     const currentArticle = categoryArticles.articles.findBy('slug', params.article_slug);
+    const category =  categoryArticles.category;
 
+    if (currentArticle.get('categories.firstObject.slug') !== category.get('slug')) {
+      return this.transitionTo('articles.category.article', currentArticle.get('categories.firstObject.slug'), currentArticle.get('slug'));
+    }
     return {
       article: currentArticle,
       category: categoryArticles.category
