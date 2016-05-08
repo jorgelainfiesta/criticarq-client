578950522597e47a8932a0999dc967c180371c84
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-05-08 06:52:24 -0600

    Fixed style on navigation.

--- app/mixins/reset-scroll-top.js
+++ app/mixins/reset-scroll-top.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  afterModel() {
+  beforeModel() {
     this._super(...arguments);
     window.scrollTo(0,0);
   }
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import ResetScrollTopMixin from 'criticarq-client/mixins/reset-scroll-top';
 
 const {
   RSVP,
   Route
 } = Ember;
 
-export default Route.extend({
+export default Route.extend(ResetScrollTopMixin, {
   model() {
     const filter = {
       tag: 'home'
