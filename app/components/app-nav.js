b126ead471ae67dbe7675f5bc065ad54e22e3136
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-05-08 07:31:43 -0600

    Fixed header.

---
+++ app/components/app-nav.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+const {
+  Component,
+  run
+} = Ember;
+
+export default Component.extend({
+  tagName: 'header',
+  classNames: ['site-nav'],
+  classNameBindings: ['fixed', 'showHeader:hide-nav'],
+  headerHeight: 80,
+  delta: 10,
+  lastPosition: 0,
+
+  init() {
+    this._super(...arguments);
+    window.onscroll = run.bind(this, this._checkScroll)
+  },
+  willDestroyElement() {
+    window.onscroll = null;
+  },
+  _checkScroll() {
+    run.throttle(this, this._addScrollClasses, 200);
+  },
+  _addScrollClasses() {
+    const currentPosition = window.scrollY;
+    const lastPosition = this.get('lastPosition');
+    const delta = this.get('delta');
+    const headerHeight = this.get('headerHeight');
+
+    if (Math.abs(lastPosition - currentPosition) < delta) {
+      return;
+    }
+
+    if (currentPosition > lastPosition  && currentPosition > headerHeight) {
+      this.set('showHeader', true);
+    } else {
+      this.set('showHeader', false);
+    }
+    this.set('lastPosition', currentPosition);
+  }
+});
