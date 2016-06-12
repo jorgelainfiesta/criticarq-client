5201bd0d68f2bc214cf94983596cbefa44d5a367
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-06-12 11:51:16 -0600

    Change model scheme for single article.

--- app/routes/articles/category.js
+++ app/routes/articles/category.js
@@ -2,28 +2,15 @@ import Ember from 'ember';
 import ResetScrollTopMixin from 'criticarq-client/mixins/reset-scroll-top';
 
 const {
-  RSVP,
   Route
 } = Ember;
 
 export default Route.extend(ResetScrollTopMixin, {
   model(params) {
-    const filter = {
-      category_name: params.category_slug
-    };
-    const articles = this.store.query('article', {
-      filter
-    });
-    const category = this.store.query('category', {
-      slug: params.category_slug
-    })
-    .then((loadedCategory) => {
-      return loadedCategory.get('firstObject');
-    });
+    const category = this.store.peekAll('category').findBy('slug', params.category_slug);
 
-    return RSVP.hash({
-      articles,
+    return {
       category
-    });
+    };
   }
 });
--- app/routes/articles/category/article.js
+++ app/routes/articles/category/article.js
@@ -2,21 +2,37 @@ import Ember from 'ember';
 import ResetScrollTopMixin from 'criticarq-client/mixins/reset-scroll-top';
 
 const {
+  get,
+  RSVP,
   Route
 } = Ember;
 
 export default Route.extend(ResetScrollTopMixin, {
   model(params) {
-    const categoryArticles = this.modelFor('articles.category');
-    const currentArticle = categoryArticles.articles.findBy('slug', params.article_slug);
-    const category =  categoryArticles.category;
-
-    if (currentArticle.get('categories.firstObject.slug') !== category.get('slug')) {
-      return this.transitionTo('articles.category.article', currentArticle.get('categories.firstObject.slug'), currentArticle.get('slug'));
-    }
-    return {
-      article: currentArticle,
-      category: categoryArticles.category
+    const { category } = this.modelFor('articles.category');
+    const filter = {
+      slug: params.article_slug
     };
+    const article = this.store.query('article', {
+      filter
+    })
+    .then((articles) => {
+      return get(articles, 'firstObject');
+    });
+
+    return RSVP.hash({
+      article,
+      category
+    });
+  },
+
+  afterModel(model) {
+    const categorySlug = get(model, 'category.slug');
+    const articleCategorySlug = get(model, 'article.categories.firstObject.slug');
+
+    if (categorySlug !== articleCategorySlug) {
+      const articleSlug = get(model, 'article.slug');
+      return this.transitionTo('articles.category.article', categorySlug, articleSlug);
+    }
   }
 });
