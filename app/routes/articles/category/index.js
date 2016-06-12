b9a51c7aec251c0d1e82c11a6f06ca5eb264232f
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-06-12 12:09:30 -0600

    Corrected category filter on category.index

--- app/routes/articles/category/index.js
+++ app/routes/articles/category/index.js
@@ -1,16 +1,19 @@
 import Ember from 'ember';
 
 const {
+  get,
   RSVP,
   Route
 } = Ember;
 
 export default Route.extend({
   model(params) {
-    const articles = this.store.query('article', {
-      slug: params.slug
-    });
     const { category } = this.modelFor('articles.category');
+    const articles = this.store.query('article', {
+      filter: {
+        category_name: get(category, 'slug')
+      }
+    });
 
     return RSVP.hash({
       category,
