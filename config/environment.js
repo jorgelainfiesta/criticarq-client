c9df7f4a547460ae4b243897c94fe758f189e511
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2018-06-27 12:55:11 -0600

    Update to Ember 3.x

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-/* jshint node: true */
+'use strict';
 
 module.exports = function(environment) {
   var ENV = {
@@ -60,6 +60,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.autoboot = false;
   }
 
   if (environment === 'production') {
